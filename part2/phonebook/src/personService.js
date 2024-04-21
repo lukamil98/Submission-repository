fd1d0311ea37c26ed68855995136b4d804d29954
lukamil1998 <luka_milanovic@rocketmail.com>
2024-04-21 23:42:40 +0200

    Fixing

--- part2/phonebook/src/personService.js
+++ part2/phonebook/src/personService.js
@@ -1,28 +1,32 @@
 import axios from "axios"
 
-const baseUrl = "https://fullstackopen-part3-ad09.onrender.com/api/persons"
+// Define the base URL for your backend API
+const backendBaseUrl =
+  "https://fullstackopen-part3-ad09.onrender.com/api/persons"
+
+// Create an instance of Axios with the base URL
+const instance = axios.create({
+  baseURL: backendBaseUrl,
+})
 
 // Function to fetch all persons from the server
 const getAll = () => {
-  return axios.get(baseUrl).then((response) => response.data)
+  return instance.get("/").then((response) => response.data)
 }
 
 // Function to add a new person to the server
 const create = (newPerson) => {
-  return axios.post(baseUrl, newPerson).then((response) => response.data)
+  return instance.post("/", newPerson).then((response) => response.data)
 }
 
 // Function to remove a person from the server by ID
 const remove = (id) => {
-  return axios.delete(`${baseUrl}/${id}`)
+  return instance.delete(`/${id}`)
 }
 
 // Function to update a person's information on the server
 const update = (id, updatedPerson) => {
-  // Send PUT request to the server to update a person's information by ID
-  return axios
-    .put(`${baseUrl}/${id}`, updatedPerson)
-    .then((response) => response.data)
+  return instance.put(`/${id}`, updatedPerson).then((response) => response.data)
 }
 
 // Object containing all the service functions
